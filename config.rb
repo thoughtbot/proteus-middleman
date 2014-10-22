94eec7bed572d91d7b998eb09d19fd756db3b622
Joshua Ogle <joshua@joshuaogle.com>
2014-10-22 12:04:22 -0600

    Clean up config comments

--- config.rb
+++ config.rb
@@ -9,30 +9,15 @@ set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :layout, 'layouts/application'
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
 configure :development do
  activate :livereload
 end
 
-# Relative assets needed to deploy to Github Pages
 configure :build do
+  # Relative assets needed to deploy to Github Pages
   activate :relative_assets
 end
 
-# Configuration options: https://github.com/karlfreeman/middleman-deploy
-# Deploys to 'origin' on a 'gh-pages' branch
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
