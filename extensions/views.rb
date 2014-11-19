747d7eaec7c29600b9b4fffd09a4ba795d68ab67
Joshua Ogle <joshua@joshuaogle.com>
2014-11-19 12:20:30 -0700

    Better Views extension

--- extensions/views.rb
+++ extensions/views.rb
@@ -2,16 +2,33 @@ class Middleman::Extensions::ViewsDirectory < Middleman::Extension
   register :views
   option   :views_dir, 'views', 'Directory for site views'
 
-  def manipulate_resource_list(resources)
-    views_directory = File.join(app.root, 'source', options.views_dir) << '/'
-    views = Dir[File.join(views_directory, '**/*')]
+  def manipulate_resource_list resources
+    views_dir = normalise options.views_dir
 
-    # Don't map directories
-    views.reject! { |path| File.directory? path }
+    # Build an array of resources from the directory
+    views = Dir.glob(views_dir + '/**/*', File::FNM_DOTMATCH).map do |sourcepath|
+      filename = sourcepath.sub(views_dir + ?/, '')
+      next if File.directory?(sourcepath) || filename[0] == "_"
+      buildpath = app.sitemap.extensionless_path filename
+      Middleman::Sitemap::Resource.new app.sitemap, buildpath, sourcepath
+    end
 
-    resources + views.map do |source|
-      view = app.sitemap.extensionless_path source[views_directory.length..-1]
-      Middleman::Sitemap::Resource.new app.sitemap, view, source
+    # Remove views directory
+    resources.reject! do |dir|
+      Dir.glob(views_dir + '/*', File::FNM_DOTMATCH).include? dir.source_file
+    end
+
+    # Add the new views to the resource list
+    resources.concat views.compact
+  end
+
+  private
+
+  def normalise(directory)
+    if Pathname(directory).relative?
+      File.expand_path File.join app.source_dir, directory
+    else
+      File.expand_path directory
     end
   end
 end
